6ef45eec5ba7897cb493f1d2533aa93cb69a6498
SuperMB <littlebass09@tamu.edu>
2023-01-10 01:57:55 -0800

    MB Initial push, ready to be imported

---
+++ Verilog/Debounce.v
@@ -0,0 +1,21 @@
+module Debounce(
+    /*[Clock 50 Mhz]*/
+    input clk,
+
+    /*[Reset]*/
+    input reset,
+
+    input button,
+
+    output debouncedButton
+);
+
+
+​​​​​/*[RisingEdge]*/
+wire risingButton;
+
+/*[Counter --count 1024 --start risingButton]*/
+reg [7:0] debounceCounter;
+
+assign debouncedButton = button || debounceCounter > 0;
+endmodule
